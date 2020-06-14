5ad4c31ea15dafa9722c94de90bd8eceebc61a82
Vladislav Zhurbenko <akel.design@inbox.ru>
2020-06-14 20:06:05 +0400

    Создан класс DataBaseItem
    Добавлен базовый функционал

---
+++ databaseitem.cpp
@@ -0,0 +1,30 @@
+#include "databaseitem.h"
+
+DataBaseItem::DataBaseItem()
+{
+    this->status=0;
+    this->value=nullptr;
+    this->id=-1;
+}
+
+DataBaseItem::DataBaseItem(QString value, int id)
+{
+    this->status=1;
+    this->value=value;
+    this->id=id;
+}
+
+void DataBaseItem::changeStatus()
+{
+    status=!status;
+}
+
+QString DataBaseItem::itemValue()
+{
+    return value;
+}
+
+int DataBaseItem::ItemId()
+{
+    return id;
+}
---
+++ databaseitem.h
@@ -0,0 +1,19 @@
+#ifndef DATABASEITEM_H
+#define DATABASEITEM_H
+#include <QString>
+
+class DataBaseItem
+{
+private:
+QString value;
+int id;
+bool status;
+public:
+    DataBaseItem();
+    DataBaseItem(QString value,int id);
+    void changeStatus();
+    QString itemValue();
+    int ItemId();
+};
+
+#endif // DATABASEITEM_H
