51ffa05c9fdb942f97cd44cc8ee8e10de5777ab3
Vladislav Zhurbenko <akel.design@inbox.ru>
2020-06-18 21:44:03 +0400

    add method render()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,12 @@ ui->sort_order->addItem("по возрастанию",0);
 ui->sort_order->addItem("по убыванию",1);
 }
 
+void MainWindow::render()
+{
+    ui->sorted_item_list->clear();
+    ui->DataBase_list->clear();
+}
+
 
 MainWindow::~MainWindow()
 {
